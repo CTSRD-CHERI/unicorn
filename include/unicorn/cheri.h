c6c829de7ad1a8da44099f5f98e96df8a66341c5
RoundofThree <zhuoyingjiangli@gmail.com>
2024-02-05 18:18:26 +0000

    Morello: add license

--- include/unicorn/cheri.h
+++ include/unicorn/cheri.h
@@ -1,4 +1,21 @@
-/* TODO: License */
+/* 
+    Unicorn Engine CHERI exports
+    Copyright (C) 2024 Zhuo Ying Jiang Li <zyj20 [at] cl.cam.ac.uk>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 #ifndef UNICORN_CHERI_H
 #define UNICORN_CHERI_H
@@ -15,11 +32,11 @@ extern "C" {
 typedef struct uc_cheri_cap {
     uint64_t address;     
     uint64_t base;        /* Capability base addr */
-    __uint128_t top;        /* Capability top, it could use 65-bits */
+    __uint128_t top;      /* Capability top, it could use 65-bits */
     uint8_t tag;          /* Tag */
     uint32_t uperms;      /* Software permissions */
     uint32_t perms;       /* Permissions */
-    uint32_t type;        /* Object type, eg. OTYPE_UNSEALED */
+    uint32_t otype;        /* Object type, eg. OTYPE_UNSEALED */
 } uc_cheri_cap;
 
 #ifdef __cplusplus
