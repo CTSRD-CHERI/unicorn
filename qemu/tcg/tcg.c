ce44158a0069f0d62021281375e32eaa71e64121
RoundofThree <zhuoyingjiangli@gmail.com>
2024-02-07 14:37:38 +0000

    Morello: fix `sync_global` to add back liveness analysis
    I mistakenly commented out debug assertions that make sure liveness analysis syncs the global at the last write.

--- qemu/tcg/tcg.c
+++ qemu/tcg/tcg.c
@@ -3723,11 +3723,11 @@ static void sync_global(TCGContext *s, TCGOp *op)
 {
     TCGTemp *ts = arg_temp(op->args[0]);
     // tcg_debug_assert(ts->kind >= TEMP_GLOBAL);
-    // tcg_debug_assert(ts->val_type == TEMP_VAL_REG ||
-    //                  ts->val_type == TEMP_VAL_MEM);
-    // // Liveness analysis should make sure that the sync happened at the last
-    // // write
-    // tcg_debug_assert((ts->val_type != TEMP_VAL_REG) || (ts->mem_coherent));
+    tcg_debug_assert(ts->val_type == TEMP_VAL_REG ||
+                     ts->val_type == TEMP_VAL_MEM);
+    // Liveness analysis should make sure that the sync happened at the last
+    // write
+    tcg_debug_assert((ts->val_type != TEMP_VAL_REG) || (ts->mem_coherent));
 }
 
 int tcg_gen_code(TCGContext *s, TranslationBlock *tb)
